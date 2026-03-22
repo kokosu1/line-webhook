1ebe389c154b6a61ac67c923eff379d5e590c329
kokosu1 <91053074+kokosu1@users.noreply.github.com>
2026-03-22 23:50:55 +0900

    main.py を更新

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 # main.py
 import uvicorn
 from webhook import app
+from scheduler import start_scheduler
+
+start_scheduler()
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=5000)
- 
